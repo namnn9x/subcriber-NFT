fa2682142d6de82f994e66bc8915fc83136218e9
tien99no1 <quoctien99no1@gmail.com>
2023-07-28 20:39:11 +0700

    hightlight page current

--- src/containers/Layout/Header/Menu.tsx
+++ src/containers/Layout/Header/Menu.tsx
@@ -1,5 +1,4 @@
-import { useNavigate } from "react-router";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const navigation = [
   { name: "Event List", href: "general-event" },
@@ -9,19 +8,24 @@ const navigation = [
 ];
 
 export const Menu = () => {
-  const navigate = useNavigate();
+  const location = useLocation();
 
   return (
     <div className="-my-6 divide-y divide-gray-500/10">
       <div className="py-6 flex items-center">
-        {navigation.map((item) => (
-          <div
-            key={item.name}
-            className="rounded-lg px-3 py-2 font-semibold text-sm leading-7 text-gray-300 hover:text-gray-50 cursor-pointer"
-          >
-            <Link to={`/${item.href}`} >{item.name}</Link>
-          </div>
-        ))}
+        {navigation.map((item) => {
+          const isActive = location.pathname === `/${item.href}`;
+          const itemClassName = `rounded-lg px-3 py-2 font-semibold text-sm leading-7 ${
+            isActive
+              ? "text-gray-50"
+              : "text-gray-400 hover:text-gray-50"
+          } cursor-pointer`;
+          return (
+            <div key={item.name} className={itemClassName}>
+              <Link to={`/${item.href}`}>{item.name}</Link>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
